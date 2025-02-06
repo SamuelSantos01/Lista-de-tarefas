060785967cf22d9b8205c51d9dd04a8c726526c3
SamuelSantos01 <samuelsantostss@gmail.com>
2025-02-06 14:38:51 -0400

    feat(): Implementação da entidade task e crianção do repositório

---
+++ to-do-list/src/main/java/com/project/to_do_list/Models/Task.java
@@ -0,0 +1,43 @@
+package com.project.to_do_list.Models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "task")
+@Table(name = "Task")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O título é obrigatório")
+    @Size(max = 100, message = "O título deve ter no máximo 100 caracteres")
+    @Column(nullable = false,length = 100, name = "title")
+    private String title;
+
+    @Size(max = 500,message = "A descrição deve ter no máximo 500 caracteres")
+    @Column(length = 500, name = "description")
+    private String description;
+
+    @Column(name = "completed")
+    private boolean completed;
+
+    @Column(name = "date")
+    private LocalDate dueDate;
+}
